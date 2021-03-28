789d0714f4ba859ca2a527edce99aa54064a5a71
Alex Manuskin <alex@manuskin.org>
2021-03-28 11:16:43 +0300

    Add basic example of deployment script

--- README.md
+++ README.md
@@ -45,3 +45,20 @@ Run the Mocha tests:
 ```sh
 $ yarn test
 ```
+
+### Deploy contract to netowrk (requires Mnemonic and infura API key)
+
+```
+npx hardhat run --network rinkeby ./scripts/deploy.ts
+```
+
+### Validate a contract with etherscan (requires API ke)
+
+```
+npx hardhat verify --network <network> <DEPLOYED_CONTRACT_ADDRESS> "Constructor argument 1"
+```
+
+### Added plugins
+
+- Gas reporter [hardhat-gas-reporter](https://hardhat.org/plugins/hardhat-gas-reporter.html)
+- Etherscan [hardhat-etherscan](https://hardhat.org/plugins/nomiclabs-hardhat-etherscan.html)
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,33 @@
 import { task } from 'hardhat/config';
 
+import { config as dotenvConfig } from 'dotenv';
+import { resolve } from 'path';
+dotenvConfig({ path: resolve(__dirname, './.env') });
+
 import { HardhatUserConfig } from 'hardhat/types';
+import { NetworkUserConfig } from 'hardhat/types';
 
 import '@nomiclabs/hardhat-waffle';
 import 'hardhat-typechain';
+import 'hardhat-gas-reporter';
+import '@nomiclabs/hardhat-etherscan';
+import path from 'path';
+import fs from 'fs';
+
+const chainIds = {
+  ganache: 1337,
+  goerli: 5,
+  hardhat: 31337,
+  kovan: 42,
+  mainnet: 1,
+  rinkeby: 4,
+  ropsten: 3,
+};
+
+const MNEMONIC = process.env.MNEMONIC || '';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
+const INFURA_API_KEY = process.env.INFURA_API_KEY || '';
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY || '';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -15,9 +39,57 @@ task('accounts', 'Prints the list of accounts', async (args, hre) => {
   }
 });
 
+function createTestnetConfig(
+  network: keyof typeof chainIds,
+): NetworkUserConfig {
+  const url: string = 'https://' + network + '.infura.io/v3/' + INFURA_API_KEY;
+  return {
+    accounts: {
+      count: 10,
+      initialIndex: 0,
+      mnemonic: MNEMONIC,
+      path: "m/44'/60'/0'/0",
+    },
+    chainId: chainIds[network],
+    url,
+  };
+}
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-export default {
-  solidity: '0.6.12',
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: MNEMONIC,
+      },
+      chainId: chainIds.hardhat,
+    },
+    goerli: createTestnetConfig('goerli'),
+    kovan: createTestnetConfig('kovan'),
+    rinkeby: createTestnetConfig('rinkeby'),
+    ropsten: createTestnetConfig('ropsten'),
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.12',
+      },
+      {
+        version: '0.6.6',
+      },
+    ],
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 100,
+    // enabled: process.env.REPORT_GAS ? true : false,
+  },
 };
+
+export default config;
