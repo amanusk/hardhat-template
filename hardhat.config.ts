ea9c03d5a38e9561aaf6f87c6c12d1a7e5908e82
amanusk <alex@manuskin.org>
2022-03-26 11:36:19 +0300

    update contract to solidity 0.8.6, update tooling to latest versions (#12)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -74,10 +74,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.6.12",
-      },
-      {
-        version: "0.6.6",
+        version: "0.8.6",
       },
     ],
   },
